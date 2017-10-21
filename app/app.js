a115a9b403fc5756cafd6d39f03217153638fb36
vivianalondo <sandrislc@gmail.com>
2017-10-21 09:42:32 -0500

    corrections test

--- app/app.js
+++ app/app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var CodeBreaker = require('./code-breaker');
 
 var app = express();
-app.set('port', (process.env.PORT || 3000));
 
 app.get('/setsecret/:secret', function (req, res) {
   number = req.params.secret;
@@ -13,10 +12,6 @@ app.get('/setsecret/:secret', function (req, res) {
 app.get('/guess/:number', function (req, res) {
   number = req.params.number;
   res.send({result: CodeBreaker.codeBreaker(number)});
-});
-
-app.listen(app.get('port'), function () {
-  console.log('NodeJs app listening on port', app.get('port'));
 });
 
 module.exports = app;
